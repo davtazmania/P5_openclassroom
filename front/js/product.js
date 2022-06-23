3b2a143b903d20ae99fc0b1cc77950fe77a226c3
davtazmania <baltus.davy@gmail.com>
2022-06-24 00:06:36 +0200

    connection de la page produit a la page index, ajout des features de chaque produit ainsi que les selecteurs de couleur

--- front/js/product.js
+++ front/js/product.js
@@ -1,15 +1,42 @@
-let params = new URL(document.location).searchParams;
-
+// recuperation de l'id
+let params = new URLSearchParams(document.location.search);
 let productId = params.get("id");
+let productList = []
 
+
+//infos des canapés 
 let title = document.getElementById("title");
 let imageUrl = document.querySelector(".item__img");
 let prix = document.getElementById("price");
 let description = document.getElementById("description");
-let selectColor = document.querySelector("colors");
+let selectColor = document.querySelector("#colors");
 let quantity = document.getElementById("quantity");
 
-fetch('http://localhost:3000/api/products')
-.then(function(product){
+  
 
+
+
+
+//fetch permet de recuperer les infos d'un seul poduit 
+fetch("http://localhost:3000/api/products/" + productId)
+.then(function(res){
+    if (res.ok) {
+        return res.json();
+    } 
+
+}).then(function(product){
+    console.log(product);
+    title.innerText = product.name;
+    prix.innerText = product.price;
+    description.innerText = product.description;
+    imageUrl.innerHTML = `<img src="${product.imageUrl}" alt="${product.altTxt}">`;
+    for (let i = 0; i < product.colors.length; i++){
+        let colorsOption = document.createElement("option");
+        console.log(product.colors);
+        colorsOption.setAttribute("value", product.colors[i]);
+        colorsOption.innerHTML = product.colors[i];
+        selectColor.appendChild(colorsOption);
+    }
 })
+
+
--- front/js/script.js
+++ front/js/script.js
@@ -1,24 +1,20 @@
-let products = [];
+let productsList = [];
 fetch('http://localhost:3000/api/products')
 .then(function(res){
     if (res.ok) {
         return res.json();
     }
 })
-.then(function(products){
+.then(function(productsList){
     let items = document.getElementById('items');
-    for(let i = 0; i < products.length; i++) {
-        items.innerHTML += `<a href="./product.html?id=${products[i].id}">
+    for(let i = 0; i < productsList.length; i++) {
+        items.innerHTML += `<a href="./product.html?id=${productsList[i]._id}">
         <article>
-         <img src="${products[i].imageUrl}" alt="${products[i].altTxt}">
-         <h3 class="productName">${products[i].name}</h3>
-         <p class="productDescription">${products[i].description}</p>
+         <img src="${productsList[i].imageUrl}" alt="${productsList[i].altTxt}">
+         <h3 class="productName">${productsList[i].name}</h3>
+         <p class="productDescription">${productsList[i].description}</p>
         </article>
        </a>`;
-       console.log(products[i]);
 };
 });
-
-
-console.log(products);
 
